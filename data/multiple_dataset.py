184d1f0f2f07573ffeafc85f20de957fda038c05
Zhehui Huang <zhehuihu@usc.edu>
2023-11-25 21:30:20 -0800

    Modify value

--- data/multiple_dataset.py
+++ data/multiple_dataset.py
@@ -27,7 +27,7 @@ class CombinedDataset(Dataset):
         self.combined_dataset_it = iter(combined_dataset)
 
     def __len__(self):
-        return int(2e6)
+        return int(1e6)
 
     def __getitem__(self, idx):
         example = next(self.combined_dataset_it)
